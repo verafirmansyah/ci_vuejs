83dc9c4e41ca85e1e9d2c73d55893e5e3e3ca3dd
Vera Firmansyah <verafirmansyah@gmail.com>
2019-01-05 11:32:31 +0700

    first

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -7,82 +7,84 @@ class User extends CI_Controller{
          $this->load->model('user_model','user');
        
     }
+    
     public function index(){
         $this->load->view('template/header');
         $this->load->view('users/index');
         $this->load->view('template/footer');
     }
-     public function showAll(){
+    
+    public function showAll(){
        $query=  $this->user->showAll();
              if($query){
                    $result['users']  = $this->user->showAll();
              }
         echo json_encode($result);
     }
-     public function addUser(){
-		$config = array(
-        array('field' => 'firstname',
-              'label' => 'Firstname',
-              'rules' => 'trim|required'
-             ),
-             array('field' => 'lastname',
-              'label' => 'Lastname',
-              'rules' => 'trim|required'
-             ),
-             array('field' => 'gender',
-              'label' => 'Gender',
-              'rules' => 'required'
-             ),
-             array('field' => 'birthday',
-              'label' => 'Birthday',
-              'rules' => 'trim|required'
-             ),
-             array('field' => 'email',
-              'label' => 'Email',
-              'rules' => 'trim|required'
-             ),
-               array(
-                'field' => 'contact',
-                'label' => 'Contact',
+    public function addUser(){
+      $config = array(
+          array('field' => 'firstname',
+                'label' => 'Firstname',
                 'rules' => 'trim|required'
                ),
-        array(
-                'field' => 'address',
-                'label' => 'Address',
+               array('field' => 'lastname',
+                'label' => 'Lastname',
                 'rules' => 'trim|required'
-        )
-);
-$this->form_validation->set_rules($config);
-        if ($this->form_validation->run() == FALSE) {
-            $result['error'] = true;
-            $result['msg'] = array(
-                'firstname'=>form_error('firstname'),
-                'lastname'=>form_error('lastname'),
-                'gender'=>form_error('gender'),
-                'birthday'=>form_error('birthday'),
-                'email'=>form_error('email'),
-                'contact'=>form_error('contact'),
-                'address'=>form_error('address')
-            );
-            
-        }else{
-                $data = array(
-                'firstname'=> $this->input->post('firstname'),
-                'lastname'=> $this->input->post('lastname'),
-                'gender'=> $this->input->post('gender'),
-                'birthday'=> $this->input->post('birthday'),
-                'email'=> $this->input->post('email'),
-                'contact'=> $this->input->post('contact'),
-                'address'=> $this->input->post('address')
-                
-            );
-            if($this->user->addUser($data)){
-               $result['error'] = false;
-                $result['msg'] ='User added successfully';
-            }
-            
-        }
-        echo json_encode($result);
+               ),
+               array('field' => 'gender',
+                'label' => 'Gender',
+                'rules' => 'required'
+               ),
+               array('field' => 'birthday',
+                'label' => 'Birthday',
+                'rules' => 'trim|required'
+               ),
+               array('field' => 'email',
+                'label' => 'Email',
+                'rules' => 'trim|required'
+               ),
+                 array(
+                  'field' => 'contact',
+                  'label' => 'Contact',
+                  'rules' => 'trim|required'
+                 ),
+          array(
+                  'field' => 'address',
+                  'label' => 'Address',
+                  'rules' => 'trim|required'
+          )
+      );
+    $this->form_validation->set_rules($config);
+      if ($this->form_validation->run() == FALSE) {
+          $result['error'] = true;
+          $result['msg'] = array(
+              'firstname'=>form_error('firstname'),
+              'lastname'=>form_error('lastname'),
+              'gender'=>form_error('gender'),
+              'birthday'=>form_error('birthday'),
+              'email'=>form_error('email'),
+              'contact'=>form_error('contact'),
+              'address'=>form_error('address')
+          );
+          
+      }else{
+              $data = array(
+              'firstname'=> $this->input->post('firstname'),
+              'lastname'=> $this->input->post('lastname'),
+              'gender'=> $this->input->post('gender'),
+              'birthday'=> $this->input->post('birthday'),
+              'email'=> $this->input->post('email'),
+              'contact'=> $this->input->post('contact'),
+              'address'=> $this->input->post('address')
+              
+          );
+          if($this->user->addUser($data)){
+             $result['error'] = false;
+              $result['msg'] ='User added successfully';
+          }
+          
+      }
+      echo json_encode($result);
     }
 
      public function updateUser(){		
